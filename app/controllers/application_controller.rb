1cc10a7ad6976d3128328dca3dabe7e889c63d7d
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-02 16:20:27 -0600

    Changed strong parameters for devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  include DeviseWhitelist
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
 end
