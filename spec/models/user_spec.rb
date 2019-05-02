7d5d883d68eed9f6c6c4242cfcf610245efcb85b
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-02 16:45:22 -0600

    Modified test for username length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,5 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_length_of(:username).is_at_least(1).is_at_most(30) }
+  it { is_expected.to validate_length_of(:username).is_at_least(1).is_at_most(50) }
 end
