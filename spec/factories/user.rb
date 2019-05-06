99fc7cce03a30d1bf25111efef98367d8883b596
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-06 07:41:08 -0600

    Added username validations

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    username { Faker::Internet.username(8).gsub(/[_\W]/, '') }
+    email  { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:email) }
-  it { should validate_length_of(:username).is_at_least(1).is_at_most(30) }
+	describe 'validations' do
+		subject { FactoryBot.create(:user) }
+
+	  it 'Validate username length' do 
+	  	should validate_length_of(:username).is_at_least(1).is_at_most(50)
+	  end
+
+	  it 'Validates unique username' do 
+	  	should validate_uniqueness_of(:username)
+	  end
+
+	  it 'Should validate presence of usernames in User Object' do
+	  	should validate_presence_of(:username)
+	  end
+  end
 end
