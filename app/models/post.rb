a10665a69cfad09e5b03635f5e5cd398349ad7ed
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-06 16:51:00 -0600

    Added unit test for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+	validates :title, length: { in: 1..100 },
+	                  presence: true
+
+	validates :content, presence: true
+
+	validates :opened, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe Post, type: :model do
     it "validate title length" do
       should validate_length_of(:title).is_at_least(1).is_at_most(100)
     end
+
+    it "validates presence of title" do
+      should validate_presence_of(:title)
+    end
+  end
+
+  describe ":content attribute validations" do
+    it "validates presence of content" do
+      should validate_presence_of(:content)
+    end
+  end
+
+  describe ":opened attribute validations" do
+    it "validates presence of opened" do
+      should validate_presence_of(:opened)
+    end
   end
 end
