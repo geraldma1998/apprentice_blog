a65d9e9d4bb68970ed6586de1e656a7adb71d0dd
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-06 15:21:59 -0600

    Added some tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_212902) do
+ActiveRecord::Schema.define(version: 2019_05_06_205146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.boolean "opened", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +29,9 @@ ActiveRecord::Schema.define(version: 2019_04_30_212902) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe ":title validations" do
+    subject { FactoryBot.create(:post) }
+    
+    it "validate title length" do
+      should validate_length_of(:title).is_at_least(1).is_at_most(100)
+    end
+  end
 end
