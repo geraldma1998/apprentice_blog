975ff498cf29004da406dd0686e8ae98b9574bf4
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-07 14:04:38 -0600

    Implemented CRUD for user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  get "users/:id", to: "users#show", as: "user"
+  resources :users
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,44 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #index" do
+
+    before do
+      get :index
+    end
+
+    it { is_expected.to render_template("index") }
+
+    it { is_expected.to render_with_layout("application") }
+
+    it { is_expected.to route(:get, "/users").to(action: :index) }
+  end
+
+  describe "GET #new" do
+
+    before do
+      get :new
+    end
+
+    it { is_expected.to render_template("new") }
+
+    it { is_expected.to render_with_layout("application") }
+  end
+
+  describe "GET #edit" do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      get :edit, params: { id: user.id }
+    end
+
+    it { is_expected.to render_template("edit") }
+
+    it { is_expected.to render_with_layout("application") }
+
+    it { is_expected.to route(:get, ("/users/" + user.id.to_s + "/edit")).to(action: :edit, id: user.id) }
+  end
+
   describe "GET #show" do
     let(:user) { FactoryBot.create(:user) }
 
