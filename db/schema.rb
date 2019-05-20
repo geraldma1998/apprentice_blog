8c777221232bd78be87c8b1cb4f4884094f53f4b
geraldma1998 <gerald.morales@pernixlabs.com>
2019-05-20 08:36:15 -0600

    Fixed schema problem x2

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2019_05_16_225055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "message", null: false
     t.bigint "user_id"
